368e6142400224ceefd8d4b7b0f1f15bb3116646
koukaa_500 <koukinabil73@gmail.com>
2024-07-10 14:11:26 +0100

    added the top design

--- front/frontend/src/app/services/authentication.service.ts
+++ front/frontend/src/app/services/authentication.service.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class AuthenticationService {
 
-  private apiUrl = 'http://10.1.1.68:3000/user'; // Replace with your backend URL
+  private apiUrl = 'http://localhost:3000/user'; // Replace with your backend URL
 
   constructor(private http: HttpClient, private storage: Storage , private router : Router) {
     this.init();
